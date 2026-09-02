2ce8def51382996a33e765175de61dd26d8176e7
nur12islam <68411946+nur12islam@users.noreply.github.com>
2026-09-03 03:22:48 +0530

    Load accessibility safeguards

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './proposal-theme.css';
+import './accessibility.css';
 import './runtime';
 
 createRoot(document.getElementById('root')!).render(
